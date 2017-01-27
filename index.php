560657d7c272e713ecb58e2b0dce23a9877fc54b
Roy Lindauer <hello@roylindauer.com>
2017-01-26 22:55:43 -0800

    add ga

--- index.php
+++ index.php
@@ -12,7 +12,18 @@ require_once 'lib/bootstrap.php';
 
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-		
+
+	  <script>
+	    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	    ga('create', 'UA-38944987-5', 'auto');
+	    ga('send', 'pageview');
+
+	  </script>
+  
 	</head>
 	<body>
 		<div class="container">
