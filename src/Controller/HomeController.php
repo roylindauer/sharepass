98f2083cbaceded8d4159702ee23242bed7d0820
Roy Lindauer <hello@roylindauer.com>
2021-06-02 23:54:44 -0700

    getting the basic crud in place

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,41 +7,72 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Secret;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use App\Repository\SecretRepository;
 
 class HomeController extends AbstractController
 {
     /**
      * @return Response
-     * @Route("/")
+     * @Route("/", name="secret_index")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('home/index.html.twig');
-    }
-
-    /**
-     * @return Response
-     * @Route("/add")
-     */
-    public function add(): Response
-    {
-        $entityManager = $this->getDoctrine()->getManager();
         $secret = new Secret();
-        $secret->setAccessKey('1234567890123');
-        $secret->setSecretData('data test');
 
-        $entityManager->persist($secret);
-        $entityManager->flush();
+        $form = $this->createFormBuilder($secret)
+            ->add('secret_data', TextareaType::class)
+            ->add('save', SubmitType::class, ['label' => 'Encrypt'])
+            ->getForm();
 
-        return $this->render('home/index.html.twig');
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $secret = $form->getData();
+            $secret->setAccessKey(uniqid());
+
+            $entityManager->persist($secret);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('secret_success', ['key' => $secret->getAccessKey()]);
+        }
+
+        return $this->render('home/index.html.twig', [
+            'form' => ($form) ? $form->createView() : false
+        ]);
+
     }
 
     /**
      * @return Response
-     * @Route("/view")
+     * @Route("/success/{key}", name="secret_success")
      */
-    public function view(): Response
+    public function success($key, Request $request)
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/success.html.twig', [
+            'link' => getenv('DOMAIN') . 'view/' . $key
+        ]);
+    }
+
+    /**
+     * @return Response
+     * @Route("/view/{key}", name="secret_view")
+     */
+    public function view($key, SecretRepository $secretRepository): Response
+    {
+        $secret = $secretRepository->findOneBy(['access_key' => $key]);
+
+        if (!$secret) {
+            throw $this->createNotFoundException(
+                'Secret Payload Does Not Exist: ' . $key
+            );
+        }
+
+        return $this->render('home/view.html.twig', [
+            'secret' => $secret->getSecretData()
+        ]);
     }
 }
